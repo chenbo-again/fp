3ee8d87e44d2a8dbc6ee90c412f8bc3a5ce997a3
chenbo <1552157226@qq.com>
2021-12-10 14:06:11 +0800

    ch5 practice

---
+++ src/practice/ch4.scala
@@ -0,0 +1,23 @@
+package practice
+
+import Option._
+
+object ch4 {
+	// 4.1
+	// Option/Option.scala
+	
+	def mean (xs: Seq[Double]): Option[Double] = {
+		if(xs.isEmpty)  None
+		else            Some(xs.sum/xs.length)
+	}
+	
+	def variance (xs: Seq[Double]): Option[Double] = {
+		
+		mean(xs) match {
+			case Some(m) => Some(xs.map(x => (x - m) * (x - m)).sum)
+			case None => None
+		}
+	}
+	
+	
+}
